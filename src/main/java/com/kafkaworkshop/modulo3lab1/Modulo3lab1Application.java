61d539b7dacdb1a651b415c378503255b355d6fc
Rodrigo Estrada <rodrigo.estrada@gmail.com>
2021-06-20 21:59:08 +0000

    feat: Spring boot initializer

---
+++ src/main/java/com/kafkaworkshop/modulo3lab1/Modulo3lab1Application.java
@@ -0,0 +1,13 @@
+package com.kafkaworkshop.modulo3lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo3lab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modulo3lab1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafkaworkshop/modulo3lab1/Modulo3lab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaworkshop.modulo3lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modulo3lab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
